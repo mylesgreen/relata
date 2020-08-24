d9812650227ade626c112df8d47be4eff4ee974e
alfredrafael <alfredrafael@gmail.com>
2020-08-23 20:47:57 -0400

    More autoplay for mobile version(s) 3

--- src/components/HeroVideo/HeroVideo.jsx
+++ src/components/HeroVideo/HeroVideo.jsx
@@ -10,7 +10,7 @@ const HeroVideo = () => {
       <video className="touch" autoPlay loop muted="muted" playsinline>
         <source src="https://relata.us/wp-content/uploads/2020/08/Myles-first-visit-back720p.mp4" />
       </video>
-      <video class="non-touch" autoPlay loop muted="muted" playsinline>
+      <video className="non-touch" autoPlay loop muted="muted" playsinline>
         <source src="https://relata.us/wp-content/uploads/2020/08/Myles-first-visit-back720p.mp4" type="video/mp4" />
       </video>
     </section>
