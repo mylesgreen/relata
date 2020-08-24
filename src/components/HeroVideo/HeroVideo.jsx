fdf34a1766572fce106c6ae0d53df4eee10c5abf
alfredrafael <alfredrafael@gmail.com>
2020-08-23 23:13:16 -0400

    Uses

--- src/components/HeroVideo/HeroVideo.jsx
+++ src/components/HeroVideo/HeroVideo.jsx
@@ -5,7 +5,42 @@ const HeroVideo = () => {
 
     return (
       <React.Fragment>
-      <div style={{background: 'black'}}>
+
+      <div dangerouslySetInnerHTML={{ __html: `
+  
+      <div style="background: black">
+        <section id="default-version-splash" class="home splash videoWrapper video" style="opacity: .7;">
+          <video
+            class="touch"
+            muted
+            autoplay
+            playsinline
+            src='https://www.alfredorafael.com/wp-content/uploads/2020/04/for-portfolio.mp4'
+          />
+      
+          <video
+            class="non-touch"
+            muted
+            autoplay
+            playsinline
+            src='https://www.alfredorafael.com/wp-content/uploads/2020/04/for-portfolio.mp4'
+          />
+        </section>
+      </div>
+  ` }}
+  />
+    
+      </React.Fragment>
+    );
+}
+
+
+export default HeroVideo;
+
+
+{/* 
+
+    <div style={{background: 'black'}}>
       <section id="default-version-splash" class="home splash videoWrapper video" style={{opacity: '.5', background: 'black'}}>
       <video className="touch" autoPlay loop muted playsinline>
         <source src="https://www.alfredorafael.com/wp-content/uploads/2020/04/for-portfolio.mp4" />
@@ -15,10 +50,5 @@ const HeroVideo = () => {
       </video>
     </section>
     </div>
-      </React.Fragment>
-    );
-}
 
-
-export default HeroVideo;
-
+*/}
