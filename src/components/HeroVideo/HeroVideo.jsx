a137e84d0fb7a15058a5bb6ca17bea3ba29f6a17
alfredrafael <alfredrafael@gmail.com>
2020-08-23 22:41:05 -0400

    Demo with Myles

--- src/components/HeroVideo/HeroVideo.jsx
+++ src/components/HeroVideo/HeroVideo.jsx
@@ -8,10 +8,10 @@ const HeroVideo = () => {
       <div style={{background: 'black'}}>
       <section id="default-version-splash" class="home splash videoWrapper video" style={{opacity: '.5', background: 'black'}}>
       <video className="touch" autoPlay loop muted playsinline>
-        <source src="https://relatacomm.files.wordpress.com/2020/08/vid_20200530_2231157291.mp4" />
+        <source src="https://www.alfredorafael.com/wp-content/uploads/2020/04/for-portfolio.mp4" />
       </video>
       <video className="non-touch" autoPlay loop muted playsinline>
-        <source src="https://relatacomm.files.wordpress.com/2020/08/vid_20200530_2231157291.mp4" type="video/mp4" />
+        <source src="https://www.alfredorafael.com/wp-content/uploads/2020/04/for-portfolio.mp4" type="video/mp4" />
       </video>
     </section>
     </div>
--- src/components/NavigationBar/NavigationBar.jsx
+++ src/components/NavigationBar/NavigationBar.jsx
@@ -29,7 +29,7 @@ const NavigationBar = () => {
         {/* Start(ish) of Navbar */}
         <div className="top-bar tb-large tb-transp">
           <div className="tb-logo">
-            <a href="https://www.relata.us/">
+            <a href="https://www.relata.us/" >
               <img src="https://relata.us/wp-content/uploads/2020/08/Screen-Shot-2020-08-19-at-2.46.59-AM.png" alt="logo" /></a>
           </div>
           <input type="checkbox" id="toggle" />
@@ -39,7 +39,6 @@ const NavigationBar = () => {
               <ul className="nav navbar-right navbar-nav">
                 <li><a href="https://www.relata.us/about/">About</a></li>
                 <li><a href="https://www.relata.us/services/">Services</a></li>
-                <li><a href="https://www.relata.us/pricing/">Pricing</a></li>
                 <li><a href="https://www.relata.us/contact/">Contact</a></li>
               </ul>
             </nav>
@@ -48,7 +47,8 @@ const NavigationBar = () => {
               <a href="https://www.jordanbpeterson.com/political-correctness/equity-when-the-left-goes-too-far/" target="_blank"><i className="fa fa-facebook" /></a>
               <a href="https://www.lawenforcementtoday.com/brooklyn-college-professor-math-reeks-of-white-supremacist-patriarchy/" target="_blank"><i className="fa fa-twitter" /></a>
               <a href="https://www.google.com/search?q=effects+of+ab5&oq=Effects+of+AB5&aqs=chrome.0.0.4066j0j1&sourceid=chrome&ie=UTF-8" target="_blank"><i className="fa fa-linkedin" /></a>
-              <a href="https://www.youtube.com/watch?v=_uk_6vfqwTA&t=1m16s" target="_blank"><i className="fa fa-vimeo-square" /></a> </div>
+          {/*     <a href="https://www.youtube.com/watch?v=_uk_6vfqwTA&t=1m16s" target="_blank"><i className="fa fa-vimeo-square" /></a> */}
+          </div>  
           </div>
         </div>
       </div>
