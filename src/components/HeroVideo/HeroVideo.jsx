cdfc822a0d6c53c5a854a42b8c008cf70e6caeaf
alfredrafael <alfredrafael@gmail.com>
2020-08-23 20:47:19 -0400

    More autoplay for mobile version(s) 2

--- src/components/HeroVideo/HeroVideo.jsx
+++ src/components/HeroVideo/HeroVideo.jsx
@@ -7,10 +7,10 @@ const HeroVideo = () => {
       <React.Fragment>
       <div style={{background: 'black'}}>
       <section id="default-version-splash" class="home splash videoWrapper video" style={{opacity: '.5', background: 'black'}}>
-      <video class="touch" autoPlay loop muted="muted" playsinline>
+      <video className="touch" autoPlay loop muted="muted" playsinline>
         <source src="https://relata.us/wp-content/uploads/2020/08/Myles-first-visit-back720p.mp4" />
       </video>
-      <video class="non-touch" autoplay="autoplay" loop="loop" muted="muted" playsinline>
+      <video class="non-touch" autoPlay loop muted="muted" playsinline>
         <source src="https://relata.us/wp-content/uploads/2020/08/Myles-first-visit-back720p.mp4" type="video/mp4" />
       </video>
     </section>
