78dc8dabdfefd3c7e7d4d39fbc9485c71dec2d93
alfredrafael <alfredrafael@gmail.com>
2020-08-26 17:03:55 -0400

    Finishes <ServiceSection/>

--- src/components/ServicesSection/ServicesSection.jsx
+++ src/components/ServicesSection/ServicesSection.jsx
@@ -52,7 +52,7 @@ const ServicesSection = () => {
               <p>                <a className="small-btn btn" href="/work/">See Our Work</a>
               </p></div>
             <div className="service">
-            <i class="fas fa-comments"></i><p />
+            <i class="fas fa-podcast"></i><p />
             <h3 className="service-head">Live Stream Broadcasting</h3>
               <hr />
               <div className="sub-service">
@@ -62,8 +62,8 @@ const ServicesSection = () => {
               <p>                <a className="small-btn btn" href="/work/?tag=strategy">See Our Work</a>
               </p></div>
             <div className="service">
-              <span className="flaticon flaticon-article" /><p />
-              <h3 className="service-head">Social Advertising</h3>
+              <i class="fas fa-bullhorn"></i><p />
+              <h3 className="service-head">Social <br/>Advertising</h3>
               <hr />
               <div className="sub-service">Logos + Brand Guides<br />
               Highlight a Key Message<br />
@@ -72,8 +72,8 @@ const ServicesSection = () => {
               <p>                <a className="small-btn btn" href="/work/?tag=branding">See Our Work</a>
               </p></div>
             <div className="service">
-              <span className="flaticon flaticon-imac" /><p />
-              <h3 className="service-head">Branding & Design</h3>
+              <i class="fas fa-palette"></i><p />
+              <h3 className="service-head">Branding <br/>& Design</h3>
               <hr />
               <div className="sub-service">
               Logos + Brand Guides<br />
@@ -85,7 +85,7 @@ const ServicesSection = () => {
             <p /></div>
           <div className="row services-listing">
             <div className="service">
-              <span className="flaticon flaticon-play43" /><p />
+              <i class="fas fa-people-arrows"></i><p />
               <h3 className="service-head">Social Media Consultation</h3>
               <hr />
               <div className="sub-service">
@@ -95,8 +95,8 @@ const ServicesSection = () => {
               <p>                <a className="small-btn btn" href="/work/?tag=video">See Our Work</a>
               </p></div>
             <div className="service">
-              <span className="flaticon flaticon-double7" /><p />
-              <h3 className="service-head">Social Media Training</h3>
+            <i class="fas fa-user-graduate"></i><p />
+            <h3 className="service-head">Social Media Training</h3>
               <hr />
               <div className="sub-service">
               Skill Up Your Team<br />
@@ -105,7 +105,7 @@ const ServicesSection = () => {
               <p>                <a className="small-btn btn" href="/work/?tag=social-media">See Our Work</a>
               </p></div>
             <div className="service">
-              <span className="flaticon flaticon-magnifier13" /><p />
+             <i class="fas fa-pencil-alt"></i><p />
               <h3 className="service-head">Ghostwriting<br />
                 Ads</h3>
               <hr />
@@ -117,7 +117,7 @@ const ServicesSection = () => {
               <p>                <a className="small-btn btn" href="/work/?tag=google">See Our Work</a>
               </p></div>
             <div className="service">
-              <span className="flaticon flaticon-email20" /><p />
+            <i class="fas fa-video"></i><p />
               <h3 className="service-head">Video & Motion Graphics</h3>
               <hr />
               <div className="sub-service">
