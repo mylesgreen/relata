16c73e1a0d8d9ef9bfe1ba5e15aeaf2b5c84c548
alfredrafael <alfredrafael@gmail.com>
2020-09-02 11:02:12 -0400

    Adds 'back to top button' at <Footer/>

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,9 +2,15 @@ import React from 'react';
 import { Col, Row, Container } from 'react-bootstrap';
 import { StyledFooter } from './StyledFooter';
 import './footer.css';
+import $ from 'jquery';
 
 
 const AppFooter = () => {
+
+    const scrollToTop = () => {
+        $(window).scrollTop(0); 
+    };
+
     return (
         <React.Fragment>
 
@@ -21,13 +27,18 @@ const AppFooter = () => {
 
 
         <Col style={{textAlign: 'center'}}>
-        <p>© 2020 Relata LLC</p>
+            <p>© 2020 Relata LLC</p>
         </Col>
 
 
 
         <Col>
-            <br/>
+        <div style={{float: 'right'}}>
+        <div 
+        className='scroll-to-top-button' 
+        onClick={scrollToTop}
+        >&nbsp;<i class="fas fa-angle-up"></i> </div>
+        </div>
         </Col>
 
         </Row>
